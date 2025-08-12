c41a31049834b7686f9fe441fe2a335208cd336e
Ndiaye Mamel Alboury <mamelalboury@gmail.com>
2025-08-12 05:33:41 +0200

    Part 3: Adding Menu Buttons

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
+var menuLinks = [   /* in part 3 */
+  { text: 'about', href: '/about' },
+  { text: 'catalog', href: '/catalog' },
+  { text: 'orders', href: '/orders' },
+  { text: 'account', href: '/account' },
+];
+
+
 /////////////* Part 1 : Getting Started ////////////////////
 
 var thisStyle = 'var(--main-bg)'
@@ -8,7 +16,7 @@ const body = document.body
 const mainEl = document.querySelector("main")
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>'
 
-console.log(mainEl.textContent);
+console.log(mainEl.textContent)
 mainEl.style.backgroundColor = thisStyle
 
 mainEl.classList.add("flex-ctr")
@@ -19,3 +27,13 @@ var thisStyle2 = 'var(--top-menu-bg)'
 const topMenuEl = document.getElementById("top-menu")
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = thisStyle2
+
+/////////////*** Part 3: Adding Menu Buttons ////////////////////
+
+let a = ""
+menuLinks.forEach(link => {
+  a = document.createElement("a")
+  a.setAttribute("href", link.href)
+  a.textContent = link.text;
+  topMenuEl.appendChild(a)
+});
